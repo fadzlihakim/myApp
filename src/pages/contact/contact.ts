76962f0bf3d822f13fc51c17148373316a91cf6b
fadzlihakim <fadzlihakim92@gmail.com>
2018-11-28 10:05:03 +0800

    fourth commit

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ActionSheetController } from 'ionic-angular';
 import { PeopleProvider } from '../../providers/people/people';
 import { DetailContactPage } from '../detail-contact/detail-contact';
 
@@ -10,10 +10,13 @@ import { DetailContactPage } from '../detail-contact/detail-contact';
 export class ContactPage {
 
   public people = [];
+  public peopleAll = [];
+  public page = 0;
+  public genderType ="";
   public errorMessage : string;
-
   public reloadData = false;
-  constructor(public navCrtl: NavController, public service:PeopleProvider,)
+
+  constructor(public navCrtl: NavController, public service:PeopleProvider, public actionSheetCtrl: ActionSheetController)
   {
     this.service.getPeopleFromApi()
     .subscribe(
@@ -47,7 +50,7 @@ export class ContactPage {
       .subscribe(
         data => this.people.push(...data["results"]),
         err => console.log(err),
-        () => e.complete
+        () => e.complete()
       )
     }
 
@@ -60,6 +63,79 @@ export class ContactPage {
   toggleReloadData()
   {
     this.reloadData = !this.reloadData
+  }
+
+  searchPerson(e)
+  {
+    console.log(e.target.value)
+    this.people = this.people.filter((person) => {
+      return person.name.first.toLowerCase().indexOf(e.target.value.toLowerCase()) > -1 ||
+      person.name.last.toLowerCase().indexOf(e.target.value.toLowerCase()) > -1;
+    })
+    console.log(this.people) 
+  }
+
+  actionFilter()
+  {
+    const actionSheet = this.actionSheetCtrl.create({
+      title: 'Filter your contact',
+      buttons: [
+        {
+          text: 'Male',
+          role: 'male',
+          handler: () => {
+            this.genderType = "male"
+            this.page = 0
+            this.service.getPeopleFilterGender(this.genderType,this.page)
+            .subscribe(
+              (response) => {
+                console.log(response);
+                this.people = response["results"]
+                this.peopleAll = this.people
+                this.page = this.page + 1
+              },
+              (error) => console.log(error)
+            )
+          }
+        },
+        {
+          text: 'Female',
+          role: 'female',
+          handler: () => {
+            this.genderType = "female"
+            this.page = 0
+            this.service.getPeopleFilterGender(this.genderType,this.page)
+            .subscribe(
+              (response) => {
+                console.log(response);
+                this.people = response["results"]
+                this.peopleAll = this.people
+                this.page = this.page + 1
+              },
+              (error) => console.log(error)
+            )
+          }
+        },
+        {
+          text: 'No Filter',
+          role: 'no filter',
+          handler: () => {
+            this.genderType = ""
+            this.page = 0
+            this.service.getPeopleFilterGender(this.genderType,this.page)
+            .subscribe(
+              (response) => {
+                console.log(response);
+                this.people = response["results"]
+                this.peopleAll = this.people
+              },
+              (error) => console.log(error)
+            )
+          }
+        }
+      ]
+    });
+    actionSheet.present();
   }
 }
 
